dde6fa750b4e3687402fdd2bbf143a573d4168c4
Sujan Tamang <suzanyba079@gmail.com>
2024-06-13 06:40:27 +0545

    exercise_1

---
+++ exercise_1.js
@@ -0,0 +1,7 @@
+//checking wheather a given value is string or not
+const value = Number(prompt("Enter your value"));
+
+console.log(typeof value);
+
+if (typeof value == "string") console.log("it is a string");
+else console.log("others");
