bd513f1b92756f2cc3051ba60fd4a3a5973f2d33
Sujan Tamang <suzanyba079@gmail.com>
2024-06-13 16:00:12 +0545

    exercise_1 updated

--- exercise_1.js
+++ exercise_1.js
@@ -1,7 +1,25 @@
 //checking wheather a given value is string or not
-const value = Number(prompt("Enter your value"));
+const value1 = prompt("Enter your value1");
+const value2 = prompt("Enter your value2");
 
-console.log(typeof value);
+const isValidValue1 =
+  (value1.charCodeAt() >= 65 && value1.charCodeAt() <= 90) ||
+  (value1.charCodeAt() >= 97 && value1.charCodeAt() <= 122);
+const isValidValue2 =
+  (value2.charCodeAt() >= 65 && value2.charCodeAt() <= 90) ||
+  (value2.charCodeAt() >= 97 && value2.charCodeAt() <= 122);
 
-if (typeof value == "string") console.log("it is a string");
-else console.log("others");
+if (isValidValue1 || isValidValue2) {
+  alert("Invalid Number");
+}
+// console.log(value.charCodeAt());
+// console.log(typeof value);
+
+// if (typeof value == "string") console.log("it is a string");
+// else console.log("others");
+
+// if (isNaN(value))
+//   alert("Invalid number");
+// }
+
+// edge cases is important
