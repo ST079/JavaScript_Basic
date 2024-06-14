a94737cd0725ed3a1887ed4583f4c64821020afb
Sujan Tamang <suzanyba079@gmail.com>
2024-06-14 07:28:22 +0545

    functions types

---
+++ functions/test.js
@@ -0,0 +1,38 @@
+// nameless function or anoymous function is a global function
+// let a = function () {
+//   console.log("heres a nameless function");
+// };
+
+//paramaterized function
+// pagination
+// const pagination = (page = 1, size = 5) => {
+//   for (let i = page; i <= size; i++) {
+//     console.log(i);
+//   }
+// };
+// pagination();
+
+//inline function is local function
+// const footer = function () {
+//   console.log("this is a footer");
+// };
+// footer(); // page 1
+// footer(); // page 2
+// concept is reausibility
+
+// closure function
+
+// const secret = () => {
+//   const password = "password";
+//   return password;
+// };
+
+// console.log(secret());
+
+// implict function
+//normal function
+const additionn = (a, b) => {
+  return a + b;
+};
+//implict function
+const addit = (a, b) => a + b;
