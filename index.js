ec18c859823da73d84c974642f0248a8a22307f4
Sujan Tamang <suzanyba079@gmail.com>
2024-06-19 16:47:22 +0545

    others

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const getFormData = () => {
-  const email = document.getElementById("email").value;
-  console.log({ email });
+// const getFormData = () => {
+//   const email = document.getElementById("email").value;
+//   console.log({ email });
+// };
+
+const getQuote = async () => {
+  const result = await fetch("https://dummyjson.com/quotes");
+  const response = await result.json();
+  console.log({ response });
+  document.getElementById("quote").innerHTML = response.quotes[0].quote;
 };
+
+getQuote();
